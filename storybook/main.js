291b45ffdb3ace4e05af7ab4c4bff6359b718904
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-25 11:08:48 +0300

    Add packages names parsing to storybook

--- storybook/main.js
+++ storybook/main.js
@@ -1,7 +1,32 @@
+const path = require('path');
+const chalk = require('chalk');
+const { argv } = require('yargs');
 const loaders = require('../webpack/loaders');
+const getPackagePath = require('../scripts/utils/get-package-path');
+
+const DEFAULT_STORIES = ['../src/**/*.story.@(jsx|mdx)'];
+const packages = argv._;
+let stories = DEFAULT_STORIES;
+
+if (packages.length !== 0) {
+  stories = [];
+
+  packages.forEach(packageName => {
+    const packagePath = getPackagePath(packageName);
+    if (packagePath) {
+      stories.push(path.join(packagePath, 'src/**/*.story.@(jsx|mdx)'));
+    } else {
+      process.stdout.write(chalk.yellow(`Unable to resolve ${packageName}`));
+    }
+  });
+}
+
+if (stories.length === 0) {
+  stories = DEFAULT_STORIES;
+}
 
 module.exports = {
-  stories: ['../src/**/*.story.@(jsx|mdx)'],
+  stories,
   webpackFinal: (config, { configType }) => {
     config.module.rules.push(loaders.less({ publicPath: '/', mode: configType.toLowerCase() }));
     return config;
